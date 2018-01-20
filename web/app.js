0da01626eab51db2f5778cb0dd133783d44b989b
Thomas Sibley <tom@zulutango.org>
2018-01-20 11:39:28 -0800

    Break up JS into multiple files
    and convert to using a real class definition for Soundbridge.  Much
    nicer!

---
+++ web/app.js
@@ -0,0 +1,38 @@
+// vim: set sw=2 ts=2 :
+(function() {
+  'use strict';
+
+  angular
+    .module('Soundbridge', ['angular.filter', 'ui.bootstrap'])
+    .config(configure)
+    .run(init);
+
+
+  // Configure $location and $anchorScroll to stay out of the way of normal URL
+  // and HTML anchors.  We don't use them for an app-wide link/routing
+  // mechanism.
+
+  configure.$inject = ['$compileProvider', '$locationProvider', '$anchorScrollProvider'];
+
+  function configure($compileProvider, $locationProvider, $anchorScrollProvider) {
+    $locationProvider.html5Mode({
+      enabled: true,
+      requireBase: false,
+      rewriteLinks: false
+    });
+    $anchorScrollProvider.disableAutoScrolling();
+  }
+
+
+  // It's super-convenient to have a global singleton always available in
+  // templates without needing to pass it around.  This means the base HTML can
+  // access it without adding a useless wrapper component or componentizing
+  // everything unnecessarily for a project like this.
+
+  init.$inject = ['Soundbridge', '$rootScope', '$log'];
+
+  function init(Soundbridge, $rootScope, $log) {
+    $rootScope.soundbridge = Soundbridge;
+  }
+
+})();
---
+++ web/components.js
@@ -0,0 +1,29 @@
+// vim: set sw=2 ts=2 :
+(function() {
+  'use strict';
+
+  angular
+    .module('Soundbridge')
+    .component('playPreset', {
+      controller: componentController,
+      templateUrl: 'partial/play-preset.html',
+      transclude: true,
+      bindings: {
+        name: '<'
+      }
+    });
+
+
+  // This is for convenience when using components, which force isolate scope,
+  // instead of directives.  Maybe I should use directives, or design the API
+  // differently… but, meh.  This is a hacky personal project, and it's
+  // super-convenient to have a global singleton always available in templates
+  // without needing to pass it around.
+
+  componentController.$inject = ['Soundbridge', '$scope'];
+
+  function componentController(Soundbridge, $scope) {
+    $scope.soundbridge = Soundbridge;
+  }
+
+})();
